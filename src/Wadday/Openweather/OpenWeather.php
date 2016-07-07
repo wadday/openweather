c25320e71f0f7b65936d258407b8958c3c2d739e
wadday <iwadday@gmail.com>
2016-07-07 18:03:45 +0500

    simple function added. to call with get(1)

--- src/Wadday/Openweather/OpenWeather.php
+++ src/Wadday/Openweather/OpenWeather.php
@@ -9,12 +9,34 @@ class OpenWeather
 	protected $key = "178c57b36348a4feedfef027a7ffd156";
 	protected $city = "1282028";
 	
-	public function get()
+	public function get($type)
+	{
+
+		if ($type == 1) {
+			return $this->simple();
+		} 
+
+	}
+
+	public function simple()
+	{
+		$data = $this->mix();
+
+		$out = $data['country_name'].", ".$data['country_code'];
+		$out .= "&nbsp;&nbsp;<i class='".$data['icon_css']."' alt='".$data['description']."' title='".$data['description']."'></i> ";
+		$out .= ",&nbsp;".$data['temperature']."°C&nbsp;";
+		$out .= ", Humidity: ".$data['humidity'];
+		$out .= ",&nbsp;".$data['day'];
+		
+		return $out;
+	}
+
+	private function mix()
 	{
 		$wcom = new wcoms($this->key, $this->city);
 		$content = $wcom->fetch();
 		$now = $wcom->data(0, $content);
 
-		return $now['icon_custom'];
+		return $now;
 	}
 }
